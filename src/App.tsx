ca1d623ad4a69c110de160f4d9779f3c4f527595
Quentin Devillers <devi.quentin@gmail.com>
2022-09-28 14:32:10 +0200

    Reorganisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import "aframe";
-import Pikachu from "./components/pikachu";
 import Assets from "./components/Assets";
+import Scene from "./components/Scene";
 
 function App() {
-
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -12,35 +11,15 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className="App">
-        <a-scene>
-          <a-assets>
-            <Assets />
-          </a-assets>
+    <div className="App">
+      <a-scene>
+        <a-assets>
+          <Assets />
+        </a-assets>
 
-          
-
-          <a-sky src="#sky"></a-sky>
-          <a-entity sound="src: #sound"></a-entity>
-          <a-entity sound="src: #sound2"></a-entity>
-
-          <a-entity
-            class="dig"
-            gltf-model="assets/images/diglett_pokemon.glb"
-            position="0 0.24989 -5.78067"
-            animation-mixer="clip:Sphere|Jump; 
-          repetitions:Infinity"
-            scale="1 1 1"
-            suis
-          ></a-entity>
-
-          <a-camera class="camera" rotation-test>
-            {mounted ? <Pikachu /> : ""}
-          </a-camera>
-        </a-scene>
-      </div>
-    </>
+        {mounted ? <Scene /> : ""}
+      </a-scene>
+    </div>
   );
 }
 

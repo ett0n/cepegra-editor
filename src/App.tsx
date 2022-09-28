6947fd40a43430a1ead8f2d4aeba68aad000a473
Quentin Devillers <devi.quentin@gmail.com>
2022-09-28 14:24:12 +0200

    Organisation des fichiers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "aframe";
+import Pikachu from "./components/pikachu";
+import Assets from "./components/Assets";
 
 function App() {
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <div className="App">
-      <a-scene>
-        <a-box src="https://i.imgur.com/mYmmbrp.jpg" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box>
-        <a-sky color="#222" src="/assets/sky2.png"></a-sky>
-      </a-scene>
-    </div>
+    <>
+      <div className="App">
+        <a-scene>
+          <a-assets>
+            <Assets />
+          </a-assets>
+
+          
+
+          <a-sky src="#sky"></a-sky>
+          <a-entity sound="src: #sound"></a-entity>
+          <a-entity sound="src: #sound2"></a-entity>
+
+          <a-entity
+            class="dig"
+            gltf-model="assets/images/diglett_pokemon.glb"
+            position="0 0.24989 -5.78067"
+            animation-mixer="clip:Sphere|Jump; 
+          repetitions:Infinity"
+            scale="1 1 1"
+            suis
+          ></a-entity>
+
+          <a-camera class="camera" rotation-test>
+            {mounted ? <Pikachu /> : ""}
+          </a-camera>
+        </a-scene>
+      </div>
+    </>
   );
 }
 

f1e1def1549975889c72dea27aedb744164ed584
Bili <jambon_bilitis@hotmail.com>
2022-10-03 14:24:01 +0200

    startScreen + footer component

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,16 +1,33 @@
+import FooterComponent from "./FooterComponent"
+import LogoComponent from "./FooterComponent"
 const StartScreen = () => {
     return (
         <>
         <div className="m-10 flex flex-col justify-center items-center">
             <h1 className="m-4 text-center justify-center">Nom du creator</h1>
-            <div className=" grid grid-cols-2 m-4">
-                <input  type="text" placeholder="Pseudo" className="input input-bordered w-full max-w-xs m-4" />
-                <input  type="mail" placeholder="Email" className="input input-bordered w-full max-w-xs m-4" />
-                <input  type="text" placeholder="Mot de passe" className="input input-bordered w-full max-w-xs m-4" />
-                <input  type="text" placeholder="Confirmer mot de passe" className="input input-bordered w-full max-w-xs m-4" />
+            {/* */}
+            <div className=" grid grid-cols-2 m-20 gap-12 shadow-lg p-20 rounded-lg">
+                <div className="grid">
+                    <label htmlFor="">Pseudo</label>
+                    <input  type="text" placeholder="Pseudo" className="input input-bordered w-full max-w-xs " />
+                </div>
+                <div className="grid">
+                    <label htmlFor="">Mail</label>
+                    <input  type="mail" placeholder="Mail" className="input input-bordered w-full max-w-xs " />
+                </div>
+                <div className="grid">
+                    <label htmlFor="">Mot de passe</label>
+                    <input  type="text" placeholder="Mot de passe" className="input input-bordered w-full max-w-xs " />
+                </div>
+                <div className="grid">
+                    <label htmlFor="">Confirmer mot de passe </label>
+                    <input  type="text" placeholder="Confirmer mot de passe " className="input input-bordered w-full max-w-xs " />
+                </div>
             </div>
             <button className="btn">Créer nouveau perso</button>
+
         </div>
+        <FooterComponent/>
         </>
     )
 }

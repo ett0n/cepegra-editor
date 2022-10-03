741cbc8c68e70627755583ee38035381b7ca486b
Bili <jambon_bilitis@hotmail.com>
2022-10-03 14:24:24 +0200

    StartScreen + footer Component

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+    return (
+        <>
+        <div className=" flex justify-evenly m-20">
+            <img src="./assets/logo-cepegra.png" alt="Logo du Cepegra" className="w-20"/>
+            <img src="./assets/logo-forem.png" alt="Logo du Forem" className="w-20"/>
+        </div>
+        
+        </>
+    )
+}
+
+export default FooterComponent

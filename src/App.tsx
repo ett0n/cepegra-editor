ea29ea8114089316edfbd870fd673342c7fe9fb5
dimitrykatana <dimitrykatana@gmail.com>
2022-10-05 09:50:25 +0200

    2versions draganddrop + userref et rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,34 @@
-/* eslint-disable */
 import * as THREE from "three";
-import * as React from "react";
 import { useRef, useState } from "react";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { useGLTF, OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useFrame, useLoader} from "@react-three/fiber";
+import { useGLTF, OrbitControls} from "@react-three/drei";
+import { TextureLoader } from "three";
 
-  // function Box(props: JSX.IntrinsicElements["mesh"]) {
-
-  //   const ref = useRef<THREE.Mesh>(null!);
-  //   // Hold state for hovered and clicked events
-  //   const [hovered, hover] = useState(false);
-  //   const [clicked, click] = useState(false);
-  //   useFrame((state, delta) => (ref.current.rotation.x += 0.01));
-
-  //   return (
-  //     <mesh {...props} ref={ref} scale={clicked ? 1.5 : 1} onClick={(event) => click(!clicked)} onPointerOver={(event) => hover(true)} onPointerOut={(event) => hover(false)}>
-  //       <boxGeometry args={[1, 1, 1]} />
-  //       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-  //     </mesh>
-  //   );
-  // }
-
-// export default function App() {
-//   return (
-//     <Canvas>
-//       <ambientLight intensity={0.5} />
-//       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-//       <pointLight position={[-10, -10, -10]} />
-//       <Box position={[-1.2, 0, 0]} />
-//       <Box position={[1.2, 0, 0]} />
-//     </Canvas>
-//   );
-// }
-
-function FirstScene() {
+function Taupiqueur() {
+  const ref = useRef<THREE.Mesh>(null!);
+  const texture = useLoader(TextureLoader,
+    '/assets/images/clouds-sky-anime.jpg ');
   const gltf = useGLTF("/assets/images/diglett_pokemon.glb");
-     const [hovered, hover] = useState(false);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+    useFrame((state, delta) => (clicked ? ref.current.rotation.y += 0.04: 0));
+
   return (
     <>
-      <primitive position= {[0, 1, 0]} object= {gltf.scene} />
-      <primitive position= {[3, 1, 0]} object= {gltf.scene.clone()} />
+    {/* sans le ref la rotation ne fonctionne pas */}
+    <mesh ref={ref} scale={clicked ? 1.5 : 1} onClick={() => click(!clicked)}>
+    <primitive position= {[0, 1, 0]} object= {gltf.scene} />
+    </mesh>
 
-      <mesh   onPointerOver={(event) => hover(true)} onPointerOut={(event) => hover(false)}
-      rotation={[-Math.PI / 2, 0, 0]} >
-        <meshStandardMaterial color={hovered ? "hotpink" : "orange"}></meshStandardMaterial>
-        <planeGeometry args={[50, 50]} />
-      </mesh>
+    <primitive  position= {[3, 1, 0]} object= {gltf.scene.clone()} />
+
+    <mesh onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} rotation={[-Math.PI / 2, 0, 0]} >
+        <meshStandardMaterial map={texture}
+        color={hovered ? "hotpink" : "red"}
+        >
+        </meshStandardMaterial>
+        <planeGeometry args={[100, 100]} />
+    </mesh>
     </>
   );
 }
@@ -55,10 +37,9 @@ export default function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <ambientLight intensity={3} />
-        <OrbitControls target={[0, 0, 0]} maxPolarAngle={1.45} />
-
-        <FirstScene></FirstScene>
+      <OrbitControls target={[1, 1, 0]} maxPolarAngle={1.45} />
+        <ambientLight intensity={2} />
+        <Taupiqueur></Taupiqueur>
       </Canvas>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// l'app de base
 import App from "./App";
+// composant drag and drop
+import DragDrop from './components/dragndrop/DragDrop';
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    {/* <DragDrop /> */}
   </React.StrictMode>
 );

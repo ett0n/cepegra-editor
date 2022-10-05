14577ddbb8aa567cd53d2f4e82ead2a1b11eaac7
Mona Lisa <blabla@hotmail.com>
2022-10-05 12:53:45 +0200

    - Récup de la scène comme dans aframe mais en Three

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Canvas, useFrame, useLoader} from "@react-three/fiber";
 import { useGLTF, OrbitControls} from "@react-three/drei";
 import { sRGBEncoding, Texture, TextureLoader } from "three";
 
+const sol = "/assets/images/grass.png"
+
 function Taupiqueur() {
   const ref = useRef<THREE.Mesh>(null!);
-  const texture = useLoader(TextureLoader,'/assets/images/grass.png');
-  const sky = useLoader(TextureLoader,'/assets/images/sky_3.png');
+  const texture = useLoader(TextureLoader, sol);
+  const day = useLoader(TextureLoader,'/assets/images/sky.png');
+  const night = useLoader(TextureLoader,'/assets/images/sky_2.png');
   //const textureElevation = useLoader(TextureLoader,'/assets/images/grass_N.png'); Elevation if we have time
   const gltf = useGLTF("/assets/images/diglett_pokemon.glb");
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-    useFrame((state, delta) => (clicked ? ref.current.rotation.y += 0.04: 0));
+    useFrame((state, delta) => (ref.current.rotation.y += 0.0015));
 
 
     // test //
@@ -33,11 +36,11 @@ function Taupiqueur() {
   return (
     <>
     {/* sans le ref la rotation ne fonctionne pas */}
-    <mesh scale={100} position={[0, 0, 0]}>
-      <meshStandardMaterial map={sky} side={THREE.DoubleSide}/>
+    <mesh  ref={ref} scale={100} position={[0, 0, 0]} onClick={() => click(!clicked)}>
+      <meshStandardMaterial map={clicked ? day : night} side={THREE.DoubleSide}/>
       <sphereGeometry/>
     </mesh>
-    <mesh ref={ref} scale={clicked ? 1.5 : 1} onClick={() => click(!clicked)}>
+    <mesh scale={clicked ? 1.5 : 1} >
         <primitive position= {[0, 1, 0]} object= {gltf.scene} />
     </mesh>
 
@@ -46,8 +49,8 @@ function Taupiqueur() {
     <mesh onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} rotation={[-Math.PI / 2, 0, 0]} >
         <meshStandardMaterial roughness={5}
         map={texture} 
-        color={"green"}
-        //color={hovered ? "limegreen" : "green"}
+        //color={"green"}
+        color={clicked ? "limegreen" : "green"}
         >
         </meshStandardMaterial>
         <planeGeometry args={[100, 100]}/>
@@ -59,7 +62,7 @@ function Taupiqueur() {
 export default function App() {
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas camera={{ fov: 50, position: [5, 5, 5] }}>
       
       <OrbitControls target={[1, 1, 0]} maxPolarAngle={1.45} />
         <ambientLight position={[0, 5, 6]} intensity={2}/>

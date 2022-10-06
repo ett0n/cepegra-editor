72d612b15c6833852c7cc66bb40a223b5540a374
LucieCamille <lcoffergeld@gmail.com>
2022-10-06 14:01:31 +0200

    comment in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import SecondConnexionScreen from "./components/SecondConnexionScreen";
 function App() {
   return (
     <div className="App">
-      {/* <a-scene>
-        <a-box src="https://i.imgur.com/mYmmbrp.jpg" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box>
-        <a-sky color="#222" src="/assets/sky2.png"></a-sky>
-      </a-scene> */}
+      {/* Dans le startscreen, prévoir reroutage vers second connexion screen si déjà connecté */}
       <SecondConnexionScreen />
 
     </div>
